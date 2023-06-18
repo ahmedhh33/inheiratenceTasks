4f532fece334fe09d8a57f1986f45217c3e7c78f
ahmedhh33 <ahmedhh3633@gmail.com>
2023-06-18 22:48:53 +0400

    animal

---
+++ Animal .cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace inheiratance
+{
+    internal class Animal
+    {
+        public string Name;
+        public int Age;
+        public Animal(string name,int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+    }
+    internal class Dog : Animal
+    {
+        string Breed;
+        string Sound;
+
+        public Dog(string name, int age, string Breed, string Sound) : base(name, age)
+        {
+            this.Breed = Breed;
+            this.Sound = Sound;
+        }
+        public void getAnimalInfo()
+        {
+            Console.WriteLine(this.Name + " " + this.Age + " " + Breed + " " + Sound);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,19 +1,24 @@
 ﻿
 using inheiratance;
+using System.Xml.Linq;
 
 Student s1 = new Student("ahmed", 25, 23332, "power system ");//task1
 s1.getInformation();
 Console.WriteLine("**************************************************");
 
 Rectangle R1 = new Rectangle("Blu", 20, 10);//task2
-Console.WriteLine("The area of rectangle = " + R1.area());//task3
+Console.WriteLine("The area of rectangle = " + R1.area());
 
 Console.WriteLine("**************************************************");
 
-Car c1 = new Car("Toyota", 2008, "Camry", 4);//task4
+Car c1 = new Car("Toyota", 2008, "Camry", 4);//task3
 c1.getviehcleinfo();
 
 Console.WriteLine("**************************************************");
 
-Manager m1 = new Manager("Salem", "AD-4443", "Operation",1200);//task5
+Manager m1 = new Manager("Salem", "AD-4443", "Operation",1200);//task4
 m1.employeeInfo();
+Console.WriteLine("**************************************************");
+
+Dog A1 = new Dog("ewit", 1, "mammals", "noid");//task5
+A1.getAnimalInfo();
