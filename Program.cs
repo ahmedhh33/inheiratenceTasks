4234f2bd36597ea3a317d13652c2f90ac186b678
ahmedhh33 <ahmedhh3633@gmail.com>
2023-06-18 22:09:10 +0400

    vehicle

--- Program.cs
+++ Program.cs
@@ -6,4 +6,9 @@ s1.getInformation();
 Console.WriteLine("**************************************************");
 
 Rectangle R1 = new Rectangle("Blu", 20, 10);//task2
-Console.WriteLine("The area of rectangle = " + R1.area());
+Console.WriteLine("The area of rectangle = " + R1.area());//task3
+
+Console.WriteLine("**************************************************");
+
+Car c1 = new Car("Toyota", 2008, "Camry", 4);
+c1.getviehcleinfo();
---
+++ Vehicle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheiratance
+{
+    internal class Vehicle
+    {
+        public string Manufacturer;
+        public int Year;
+        public Vehicle(string manumacture, int year) 
+        {
+            Manufacturer = manumacture;
+            Year = year;
+        }
+    }
+
+    internal class Car :Vehicle
+    {
+        string model;
+        int numberOfDoor;
+
+        public Car(string manumacture, int year, string model, int numberofdoor) : base(manumacture, year)
+        {
+            this.model = model;
+            numberOfDoor = numberofdoor;
+        }
+
+        public void getviehcleinfo()
+        {
+            Console.WriteLine(Manufacturer + " " + Year + " " + model + " " + numberOfDoor);
+        }
+    }
+}
