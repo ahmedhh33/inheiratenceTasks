57200df6a7dea584fb98921e8c5ae9cf3a3eb27d
ahmedhh33 <ahmedhh3633@gmail.com>
2023-06-18 22:24:38 +0400

    employee

---
+++ Employee .cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheiratance
+{
+    internal class Employee
+    {
+        public string name { get; set; }
+        public string employeeid { get; }
+        public Employee(string name, string employeeid)
+        {
+            this.name = name;
+            this.employeeid = employeeid;
+        }
+    }
+    internal class Manager : Employee
+    {
+        string departement { get; set; }
+        private double salary { get; }
+        public Manager(string name, string employeeid, string departement, double salary) : base(name, employeeid)
+        {
+            this.departement = departement;
+            this.salary = salary;
+        }
+
+        public void employeeInfo()
+        {
+            Console.WriteLine(name+" "+employeeid+" "+departement+" "+salary);
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,5 +10,10 @@ Console.WriteLine("The area of rectangle = " + R1.area());//task3
 
 Console.WriteLine("**************************************************");
 
-Car c1 = new Car("Toyota", 2008, "Camry", 4);
+Car c1 = new Car("Toyota", 2008, "Camry", 4);//task4
 c1.getviehcleinfo();
+
+Console.WriteLine("**************************************************");
+
+Manager m1 = new Manager("Salem", "AD-4443", "Operation",1200);//task5
+m1.employeeInfo();
